dc6bc9e305f09a0da510611fe013762687b414f7
zhouzhitong <528382226@qq.com>
2020-09-04 09:46:52 +0800

    初始化-0.0.1

---
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/Main.java
@@ -0,0 +1,17 @@
+package com.zzt;
+
+/**
+ * 描述：<br>
+ * </>
+ *
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/9/4 9:42
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!!!");
+    }
+
+}
---
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/Main.java
@@ -0,0 +1,12 @@
+package com.zzt.algorithm;
+
+/**
+ * 描述：<br>
+ * </>
+ *
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/9/4 9:43
+ */
+public class Main {
+}
