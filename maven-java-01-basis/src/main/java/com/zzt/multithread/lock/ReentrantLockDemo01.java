45fbc862658d5af0e84a333640f2a50cbd1e45c2
ZZt012818 <528382226@qq.com>
2020-09-06 10:42:49 +0800

    多线程操作 --- 锁 --- LockDemo01

--- maven-java-01-basis/src/main/java/com/zzt/multithread/lock/ReentrantLockDemo01.java
+++ maven-java-01-basis/src/main/java/com/zzt/multithread/lock/ReentrantLockDemo01.java
@@ -13,18 +13,39 @@ import java.util.concurrent.locks.ReentrantLock;
  **/
 public class ReentrantLockDemo01 {
 
-    public static void main(String[] args) {
-        Lock lock = new ReentrantLock();
+    public static void main(String[] args) throws InterruptedException {
+        LockThread lockThread = new LockThread();
+        Thread thread1 = new Thread(lockThread);
+        Thread thread2 = new Thread(lockThread);
+        Thread thread3 = new Thread(lockThread);
+        thread1.start();
+        thread2.start();
+        thread3.start();
 
-        for (int i = 0; i < 3; i++) {
-            lock.lock();
-        }
-        System.out.println("1");
-        for (int i = 0; i < 2; i++) {
-            lock.unlock();
-        }
-        System.out.println("2");
+        thread1.join();
+        thread2.join();
+        thread3.join();
 
+        System.out.println(lockThread.count);
     }
-
 }
+
+class LockThread implements Runnable {
+    Integer count = 0;
+    private Lock lock = new ReentrantLock();
+
+    @Override
+    public void run() {
+        for (int i = 0; i < 10; i++) {
+            try {
+                lock.lock();
+                count++;
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+        }
+    }
+}
+
