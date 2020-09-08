573c9745e671338db2212a9877a1d31a2f054333
zhouzhitong <528382226@qq.com>
2020-09-08 18:08:30 +0800

    java基础 -- NIO -- 通道channel

--- maven-java-01-basis/src/main/java/com/zzt/nio/channel/FileChannelDemo01.java
+++ maven-java-01-basis/src/main/java/com/zzt/nio/channel/FileChannelDemo01.java
@@ -1,5 +1,7 @@
 package com.zzt.nio.channel;
 
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.io.File;
@@ -7,7 +9,11 @@ import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.nio.ByteBuffer;
+import java.nio.MappedByteBuffer;
 import java.nio.channels.FileChannel;
+import java.nio.channels.FileChannel.*;
+import java.nio.file.Paths;
+import java.nio.file.StandardOpenOption;
 
 /**
  * 描述：<br>
@@ -19,11 +25,62 @@ import java.nio.channels.FileChannel;
  */
 public class FileChannelDemo01 {
 
-    private String filePath = "C:\\Users\\Admin\\Desktop\\test.txt";
-    private String filePath2 = "C:\\Users\\Admin\\Desktop\\aaa.txt";
+    private String filePath = "D:\\BaiduNetdiskDownload\\尚硅谷JDK8新特性+JUC+NIO\\juc\\juc.zip";
+    private String filePath2 = "D:\\aaa.zip";
+
+    private static long start;
+
+    @BeforeAll
+    static void init() {
+        start = System.currentTimeMillis();
+    }
+
+    @AfterAll
+    static void destroy() {
+        System.out.println("总共耗费时间：--> " + (System.currentTimeMillis() - start));
+    }
 
     @Test
-    public void test1() throws IOException {
+    public void test2() throws IOException {// 1372ms - 1271ms
+        FileChannel read = null;
+        FileChannel write = null;
+        try {
+            read = FileChannel.open(Paths.get(filePath), StandardOpenOption.READ);
+            write = FileChannel.open(Paths.get(filePath2), StandardOpenOption.WRITE, StandardOpenOption.READ, StandardOpenOption.CREATE);
+
+            MappedByteBuffer readMap = read.map(MapMode.READ_ONLY, 0, read.size());
+            MappedByteBuffer writeMap = write.map(MapMode.READ_WRITE, 0, read.size());
+            byte[] dst = new byte[readMap.limit()];
+            readMap.get(dst);
+            writeMap.put(dst);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            read.close();
+            write.close();
+        }
+
+
+    }
+
+    @Test
+    public void test21() throws IOException {
+        FileChannel read = null;
+        try {
+            read = FileChannel.open(Paths.get(filePath), StandardOpenOption.READ);
+            MappedByteBuffer map = read.map(MapMode.READ_ONLY, 0, read.size());
+            byte[] bytes = new byte[map.limit()];
+            map.get(bytes);
+            System.out.println(new String(bytes));
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            read.close();
+        }
+    }
+
+    @Test
+    public void test1() throws IOException { // 10240ms - 10812ms
         File file = new File(filePath);
         File file2 = new File(filePath2);
         FileInputStream fileInputStream = new FileInputStream(file);
@@ -31,7 +88,7 @@ public class FileChannelDemo01 {
         FileChannel iChannel = fileInputStream.getChannel();
         FileChannel oChannel = fileOutputStream.getChannel();
 
-        ByteBuffer buffer = ByteBuffer.allocate(100);
+        ByteBuffer buffer = ByteBuffer.allocateDirect(1024);
         while ((iChannel.read(buffer)) != -1) {
             buffer.flip();
             oChannel.write(buffer);
@@ -42,7 +99,6 @@ public class FileChannelDemo01 {
         oChannel.close();
         fileInputStream.close();
         fileOutputStream.close();
-
     }
 
 }
