48c7f98b28e729e4c51b388691362352f4dd2c77
zhouzhitong <528382226@qq.com>
2020-09-08 12:02:37 +0800

    算法 -- LeetCode -- 2020/9/8

---
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code/day_20_09_08/Solution01.java
@@ -0,0 +1,28 @@
+package com.zzt.algorithm.leet_code.day_20_09_08;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * 描述：<br>
+ * </>
+ *
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/9/8 11:58
+ */
+public class Solution01 {
+
+    private List<List<Integer>> result;
+
+    public List<List<Integer>> combine(int n, int k) {
+        result = new ArrayList<>();
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        int n = 4, k = 2;
+    }
+
+}
