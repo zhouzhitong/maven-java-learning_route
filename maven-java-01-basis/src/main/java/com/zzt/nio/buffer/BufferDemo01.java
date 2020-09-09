62237815fd9c73dd8b685d1897d8d399c0783233
zhouzhitong <528382226@qq.com>
2020-09-09 12:00:55 +0800

    java基础 -- NIO -- 缓存Buffer补充

--- maven-java-01-basis/src/main/java/com/zzt/nio/buffer/BufferDemo01.java
+++ maven-java-01-basis/src/main/java/com/zzt/nio/buffer/BufferDemo01.java
@@ -6,6 +6,7 @@ import java.nio.ByteBuffer;
 
 /**
  * 描述：<br>
+ * 说明：https://blog.csdn.net/First_Bal/article/details/108463263
  * </>
  *
  * @author 周志通
@@ -13,6 +14,36 @@ import java.nio.ByteBuffer;
  * @date 2020/9/8 10:51
  */
 public class BufferDemo01 {
+
+    @Test
+    public void test01() {
+        // 1. 准备好Buffer -- Buffer的容量
+        ByteBuffer byteBuffer = ByteBuffer.allocate(1024);
+        // 2. 准备数据
+        String str = "6212261001100235339\n" +
+                "360425199809012818\n" +
+                "1. 基于Java EE技术的学校的问卷调查系统的设计与实现\n" +
+                "2. 基于B/S架构的工厂物料管理系统的设计与实现";
+        byteBuffer.put(str.getBytes());
+        // 理论：str.getBytes().length = position
+        System.out.println("验证写入的数据大小和position的关系：" + (str.getBytes().length) + " -- " + (byteBuffer.position()));
+        System.out.println(byteBuffer.limit());
+        // 3. 测试：开始读模式1
+        byteBuffer.flip();
+        byte[] bytes = new byte[20];
+        // 第一次读取
+        byteBuffer.get(bytes);
+        System.out.println("读取结果：" + new String(bytes));
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+        // 第二次读取
+        byteBuffer.get(bytes);
+        System.out.println("读取结果：" + new String(bytes));
+        System.out.println(byteBuffer.limit());
+        System.out.println(byteBuffer.position());
+
+        byteBuffer.clear();
+    }
 
     @Test
     public void test() {
