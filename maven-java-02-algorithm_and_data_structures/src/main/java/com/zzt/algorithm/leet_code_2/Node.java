2aaccbb4314a145da6d7ed9c67a9d33842be49ae
ZZt012818 <528382226@qq.com>
2020-09-29 00:08:56 +0800

    算法 -- LeetCode -- 117. 填充每个节点的下一个右侧节点指针 II

---
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_2/Node.java
@@ -0,0 +1,30 @@
+package com.zzt.algorithm.leet_code_2;
+
+/**
+ * 描述：<br>
+ * </>
+ *
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/9/28 16:27
+ **/
+public class Node {
+    public int val;
+    public Node left;
+    public Node right;
+    public Node next;
+
+    public Node() {
+    }
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, Node _left, Node _right, Node _next) {
+        val = _val;
+        left = _left;
+        right = _right;
+        next = _next;
+    }
+};
---
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_2/Solution_117.java
@@ -0,0 +1,106 @@
+package com.zzt.algorithm.leet_code_2;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+/**
+ * 描述：<br> 117. 填充每个节点的下一个右侧节点指针 II
+ * 网址：https://leetcode-cn.com/problems/populating-next-right-pointers-in-each-node-ii/solution/
+ * </>
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/9/28 16:27
+ **/
+public class Solution_117 {
+
+    public Node connect(Node root) {
+        if (root == null) {
+            return null;
+        }
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(root);
+        while (!queue.isEmpty()) {
+            //每一层的数量
+            int levelCount = queue.size();
+            //前一个节点
+            Node pre = null;
+            for (int i = 0; i < levelCount; i++) {
+                //出队
+                Node node = queue.poll();
+                //如果pre为空就表示node节点是这一行的第一个，
+                //没有前一个节点指向他，否则就让前一个节点指向他
+                if (pre != null) {
+                    pre.next = node;
+                }
+                //然后再让当前节点成为前一个节点
+                pre = node;
+                //左右子节点如果不为空就入队
+                if (node.left != null) {
+                    queue.add(node.left);
+                }
+                if (node.right != null) {
+                    queue.add(node.right);
+                }
+            }
+        }
+        return root;
+    }
+
+    /*public Node connect(Node root) {
+        if (root == null)
+            return root;
+        //cur我们可以把它看做是每一层的链表
+        Node cur = root;
+        while (cur != null) {
+            //遍历当前层的时候，为了方便操作在下一
+            //层前面添加一个哑结点（注意这里是访问
+            //当前层的节点，然后把下一层的节点串起来）
+            Node dummy = new Node(0);
+            //pre表示访下一层节点的前一个节点
+            Node pre = dummy;
+            //然后开始遍历当前层的链表
+            while (cur != null) {
+                if (cur.left != null) {
+                    //如果当前节点的左子节点不为空，就让pre节点
+                    //的next指向他，也就是把它串起来
+                    pre.next = cur.left;
+                    //然后再更新pre
+                    pre = pre.next;
+                }
+                //同理参照左子树
+                if (cur.right != null) {
+                    pre.next = cur.right;
+                    pre = pre.next;
+                }
+                //继续访问这一行的下一个节点
+                cur = cur.next;
+            }
+            //把下一层串联成一个链表之后，让他赋值给cur，
+            //后续继续循环，直到cur为空为止
+            cur = dummy.next;
+        }
+        return root;
+    }*/
+
+    public void levelOrder(TreeNode tree) {
+        if (tree == null)
+            return;
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.add(tree);//相当于把数据加入到队列尾部
+        while (!queue.isEmpty()) {
+            //poll方法相当于移除队列头部的元素
+            TreeNode node = queue.poll();
+            System.out.println(node.val);
+            if (node.left != null) {
+                queue.add(node.left);
+            }
+            if (node.right != null) {
+                queue.add(node.right);
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+
+    }
+}
