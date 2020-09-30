396bcd8c7ebf0115ee37f51c140f245132653f1c
ZZt012818 <528382226@qq.com>
2020-09-30 10:55:45 +0800

    二叉树的应用 --- 二叉树：查找当前节点的后继节点（中序遍历，的后一个节点）

--- maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/data_structures/tree/application/TreeApplicationDemo02.java
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/data_structures/tree/application/TreeApplicationDemo02.java
@@ -1,9 +1,9 @@
 package com.zzt.data_structures.tree.application;
 
 /**
- * 描述：<br>二叉树：查找当前节点的后继节点（中序遍历，的后一个节点）
+ * 描述：<br>二叉树：查找当前节点的后继节点（中序遍历，的后一个节点）、前继节点
  * 条件：
- *      1. 每个节点，存在 左右子节点信息，和父节点信息。
+ * 1. 每个节点，存在 左右子节点信息，和父节点信息。
  * </>
  *
  * @author 周志通
@@ -12,6 +12,36 @@ package com.zzt.data_structures.tree.application;
  **/
 public class TreeApplicationDemo02 {
 
+    /**
+     * 求前继节点
+     */
+    public static Node getPredecessorNode(Node node) {
+        if (node == null) {
+            return null;
+        }
+        if (node.left != null) {
+            return getRightMost(node);
+        } else {
+            Node parent = node.parent;
+            while (parent != null && parent.right != node) {
+                node = parent.parent;
+                parent = node.parent;
+            }
+            return node;
+        }
+
+    }
+
+    private static Node getRightMost(Node node) {
+        while (node.right != null) {
+            node = node.right;
+        }
+        return node;
+    }
+
+    /**
+     * 求后继节点
+     */
     public static Node getSuccessor(Node node) {
         if (node == null) {
             return null;
