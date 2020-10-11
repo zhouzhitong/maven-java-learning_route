fac0279276bdb81a7d71d4e0bf39a94ce5957e2c
ZZt012818 <528382226@qq.com>
2020-10-11 20:46:56 +0800

    算法

--- maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_3/Solution_416.java
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_3/Solution_416.java
@@ -1,8 +1,10 @@
 package com.zzt.algorithm.leet_code_3;
 
+import java.util.Arrays;
+
 /**
  * 描述：<br> 416. 分割等和自己
- *     网址：https://leetcode-cn.com/problems/partition-equal-subset-sum/
+ * 网址：https://leetcode-cn.com/problems/partition-equal-subset-sum/
  * </>
  *
  * @author 周志通
@@ -11,7 +13,39 @@ package com.zzt.algorithm.leet_code_3;
 public class Solution_416 {
 
     public boolean canPartition(int[] nums) {
-
+        Arrays.sort(nums);
+        int sum = 0;
+        int partition;
+        for (int i = 0; i < nums.length; i++) {
+            sum += nums[i];
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        partition = sum / 2;
+        if (nums[nums.length - 1] > partition) {
+            return false;
+        } else if (nums[nums.length - 1] == partition) {
+            return true;
+        }
+        partition = partition - nums[nums.length - 1];
+        for (int i = nums.length - 1; i >= 0; i--) {
+            if (nums[i] <= partition) {
+                int copy = partition;
+                copy -= nums[i];
+                if (copy == 0) {
+                    return true;
+                }
+                for (int j = i - 1; j >= 0; j--) {
+                    if (nums[j] <= copy) {
+                        copy -= nums[j];
+                        if (copy == 0) {
+                            return true;
+                        }
+                    }
+                }
+            }
+        }
         return false;
     }
 
