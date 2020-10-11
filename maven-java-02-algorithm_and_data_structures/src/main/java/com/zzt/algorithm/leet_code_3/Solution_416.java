ee97ba75d40d31b7a16f58cfdcdcf05a24250de2
ZZt012818 <528382226@qq.com>
2020-10-11 21:26:46 +0800

    算法

--- maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_3/Solution_416.java
+++ maven-java-02-algorithm_and_data_structures/src/main/java/com/zzt/algorithm/leet_code_3/Solution_416.java
@@ -3,7 +3,7 @@ package com.zzt.algorithm.leet_code_3;
 import java.util.Arrays;
 
 /**
- * 描述：<br> 416. 分割等和自己
+ * 描述：<br> 416. 分割等和子集
  * 网址：https://leetcode-cn.com/problems/partition-equal-subset-sum/
  * </>
  *
@@ -11,13 +11,50 @@ import java.util.Arrays;
  * @date 2020/10/11 14:16
  **/
 public class Solution_416 {
+    private int[] nums;
 
     public boolean canPartition(int[] nums) {
         Arrays.sort(nums);
+        this.nums = nums;
         int sum = 0;
         int partition;
-        for (int i = 0; i < nums.length; i++) {
-            sum += nums[i];
+        for (int num : nums) {
+            sum += num;
+        }
+        if (sum % 2 != 0) {
+            return false;
+        }
+        partition = sum / 2;
+        if (nums[nums.length - 1] > partition) {
+            return false;
+        } else if (nums[nums.length - 1] == partition) {
+            return true;
+        }
+//        partition = partition - nums[nums.length - 1];
+        return dfs(partition, nums.length - 1);
+    }
+
+    private boolean dfs(int partition, int i) {
+        if (partition == 0) {
+            return true;
+        } else if (partition < 0) {
+            return false;
+        }
+        for (int j = i; j >= 0; j--) {
+            boolean dfs = dfs(partition - this.nums[j], j - 1);
+            if (dfs) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /*public boolean canPartition(int[] nums) {
+        Arrays.sort(nums);
+        int sum = 0;
+        int partition;
+        for (int num : nums) {
+            sum += num;
         }
         if (sum % 2 != 0) {
             return false;
@@ -47,10 +84,13 @@ public class Solution_416 {
             }
         }
         return false;
-    }
+    }*/
 
     public static void main(String[] args) {
-
+//        int[] nums = {1, 5, 11, 5};
+        int[] nums = {100, 100, 100, 100, 100, 100, 99, 97};
+        boolean b = new Solution_416().canPartition(nums);
+        System.out.println(b);
     }
 
 }
