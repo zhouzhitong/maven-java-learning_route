ec7d8239827f89f9e2583e06eac6f4c0eeb65ae7
zhouzhitong <528382226@qq.com>
2020-11-03 09:57:44 +0800

    算法

---
+++ maven-java-01-design_patterns/src/main/java/com/zzt/structureType/bridging/demo02/ClientDemo02.java
@@ -0,0 +1,17 @@
+package com.zzt.structureType.bridging.demo02;
+
+import java.io.File;
+
+/**
+ * 描述：<br>
+ * </>
+ *
+ * @author 周志通
+ * @version 1.0.0
+ * @date 2020/11/2 17:46
+ */
+public class ClientDemo02 {
+    public static void main(String[] args) {
+
+    }
+}
