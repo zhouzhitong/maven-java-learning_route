e725fcf51dad6874508817a202dc7f855528528c
zhouzhitong <528382226@qq.com>
2020-11-17 15:46:15 +0800

    Spring扩展 -- 自定义标签

--- maven-java-08-spring-code_extend/src/main/java/com/zzt/spring/SpringCodeExtendMain.java
+++ maven-java-08-spring-code_extend/src/main/java/com/zzt/spring/SpringCodeExtendMain.java
@@ -3,6 +3,7 @@ package com.zzt.spring;
 import com.zzt.spring.bean.TestBean;
 import com.zzt.spring.context.MyClassPathXmlApplicationContext;
 import com.zzt.spring.myTag.User;
+import org.junit.jupiter.api.Test;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
@@ -15,15 +16,19 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
  * @date 2020/10/27 9:20
  */
 public class SpringCodeExtendMain {
+
     public static void main(String[] args) {
-
         ApplicationContext applicationContext = new MyClassPathXmlApplicationContext("application.xml");
-
-        User user = applicationContext.getBean("user",User.class);
-        System.out.println(user);
-
 //        User testBean = (User) applicationContext.getBean("1108");
 //        System.out.println(testBean);
-
     }
+
+    @Test
+    public void selfTagTest() {
+        ApplicationContext applicationContext = new ClassPathXmlApplicationContext("/selfTagApplication.xml");
+        User user = applicationContext.getBean("user", User.class);
+        System.out.println(user);
+    }
+
+
 }
