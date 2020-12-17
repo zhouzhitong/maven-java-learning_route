ea046564efc88fbfa1717fd5f9647b14d6351639
ZZt012818 <528382226@qq.com>
2020-12-17 22:39:27 +0800

    spring - aop

--- maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/aop/AopTest.java
+++ maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/aop/AopTest.java
@@ -1,10 +1,25 @@
 package com.zzt.aop;
 
+import com.zzt.aop.service.Calculator;
+import com.zzt.aop.service.MyCalculator;
+import com.zzt.aop.service.SecondCalculator;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
 /**
- * 描述：<br>
- * </>
- *
- * @author zzt
- */
+ 描述：<br>
+ </>
+ @author zzt */
 public class AopTest {
+    public static void main(String[] args) throws Exception {
+        ApplicationContext ac = new ClassPathXmlApplicationContext("aopBean.xml");
+        MyCalculator bean = ac.getBean("myCalculator", MyCalculator.class);
+        Calculator calculator = (Calculator) ac.getBean("secondCalculator");
+
+        Integer add = bean.add(1, 1);
+        System.out.println(add);
+
+        Integer mul = calculator.mul(2, 4);
+        System.out.println(mul);
+    }
 }
