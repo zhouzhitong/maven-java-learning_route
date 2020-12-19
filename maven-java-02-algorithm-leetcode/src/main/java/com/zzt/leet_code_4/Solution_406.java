de82a6e7ae50ab0b3d5758f28de1b833869a988a
zhouzhitong <528382226@qq.com>
2020-12-19 14:44:45 +0800

    算法 -- 贪心算法

--- maven-java-02-algorithm-leetcode/src/main/java/com/zzt/leet_code_4/Solution_406.java
+++ maven-java-02-algorithm-leetcode/src/main/java/com/zzt/leet_code_4/Solution_406.java
@@ -14,11 +14,55 @@ import java.util.concurrent.atomic.AtomicInteger;
  *
  * @author 周志通
  * @version 1.0.0
- * @date 2020/11/16 14:14
  */
 public class Solution_406 {
 
+    @Test
+    public void test01() {
+        int[][] people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
+        int[][] ints = reconstructQueue(people);
+        if (ints != null) {
+            for (int[] anInt : ints) System.out.println(Arrays.toString(anInt));
+        }
+    }
+
     public int[][] reconstructQueue(int[][] people) {
+        quickSort(people, 0, people.length - 1);
+        List<int[]> list = new ArrayList<>();
+        for (int[] p : people) list.add(p[1], p);
+        return list.toArray(new int[0][2]);
+    }
+
+    private void quickSort(int[][] arr, int left, int right) {
+        if (left >= right) return;
+        int pivot = partition(arr, left, right);
+        quickSort(arr, left, pivot - 1);
+        quickSort(arr, pivot + 1, right);
+    }
+
+    private int partition(int[][] arr, int l, int r) {
+        int[] pivot = arr[l];
+        while (l < r) {
+            while (l < r) {
+                if (arr[r][0] > pivot[0] || arr[r][0] == pivot[0] && arr[r][1] < pivot[1]) {
+                    arr[l++] = arr[r];
+                    break;
+                }
+                r--;
+            }
+            while (l < r) {
+                if (arr[l][0] < pivot[0] || arr[l][0] == pivot[0] && arr[l][1] > pivot[1]) {
+                    arr[r--] = arr[l];
+                    break;
+                }
+                l++;
+            }
+        }
+        arr[l] = pivot;
+        return l;
+    }
+
+    /*public int[][] reconstructQueue(int[][] people) {
         Arrays.sort(people, (v1, v2) -> {
             int i = v1[1] - v2[1];
             if (i == 0) {
@@ -46,18 +90,8 @@ public class Solution_406 {
             }
             result.add(i, person);
         }
-        AtomicInteger i = new AtomicInteger();
-        result.forEach(a -> people[i.getAndIncrement()] = a);
+        for (int i = 0; i < result.size(); i++) people[i] = result.get(i);
         return people;
-    }
-
-    @Test
-    public void test01() {
-        int[][] people = {{7, 0}, {4, 4}, {7, 1}, {5, 0}, {6, 1}, {5, 2}};
-        int[][] ints = new Solution_406().reconstructQueue(people);
-        if (ints != null) {
-            for (int[] anInt : ints) System.out.println(Arrays.toString(anInt));
-        }
-    }
+    }*/
 
 }
