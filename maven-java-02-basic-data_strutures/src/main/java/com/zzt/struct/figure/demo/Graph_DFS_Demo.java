d2647ff9146bb73a24693bad1f2603beb77d0f10
ZZt012818 <528382226@qq.com>
2020-12-27 13:48:16 +0800

    算法 -- 图结构 -- 基础 -- 深度优先遍历

--- maven-java-02-basic-data_strutures/src/main/java/com/zzt/struct/figure/demo/Graph_DFS_Demo.java
+++ maven-java-02-basic-data_strutures/src/main/java/com/zzt/struct/figure/demo/Graph_DFS_Demo.java
@@ -4,15 +4,52 @@ import com.zzt.struct.figure.struct.Node;
 
 import java.util.HashSet;
 import java.util.Set;
+import java.util.Stack;
 
 /**
- 描述：<br> 图结构的宽度优先遍历
+ 描述：<br> 图结构的深度优先遍历
+ 1. 递归实现
+ @see #dfs(Node)
+ 2. 栈结构实现
+ @see #dfsTraverse(Node)
  </>
  @author 周志通
  @version 1.0.0
  @date 2020/12/27 9:36 **/
 public class Graph_DFS_Demo {
 
+    /**
+     【栈结构实现】
+     @param node
+     */
+    public static void dfsTraverse(Node<?> node) {
+        if (node == null) {
+            return;
+        }
+        Stack<Node<?>> stack = new Stack<>();
+        Set<Node<?>> set = new HashSet<>();
+        System.out.println(node);
+        stack.push(node);
+        set.add(node);
+        while (!stack.isEmpty()) {
+            Node<?> cur = stack.pop();
+            for (Node<?> next : cur.nexts) {
+                if (!set.contains(next)) {
+                    System.out.println(next.data);
+                    stack.push(cur);
+                    stack.push(next);
+                    set.add(next);
+                    break;
+                }
+            }
+        }
+    }
+
+
+    /**
+     【递归实现】
+     @param node
+     */
     public static void dfs(Node<?> node) {
         doDfs(node, new HashSet<>());
     }
