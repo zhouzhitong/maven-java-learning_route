31ec67e48ffe5dfaa49a5ac1aaaa45cdf080298e
ZZt012818 <528382226@qq.com>
2020-12-30 23:21:09 +0800

    算法 -- 暴力递归

---
+++ maven-java-02-basic-data_strutures/src/main/java/com/zzt/struct/recursion/application01/HanoTa.java
@@ -0,0 +1,107 @@
+package com.zzt.struct.recursion.application01;
+
+/**
+ 描述：<br>汉罗塔问题
+ </>
+ @author 周志通
+ @version 1.0.0
+ @date 2020/12/30 23:13 **/
+public class HanoTa {
+
+    // 方式1：分7步
+    // 步骤1：
+    public static void hanoi1(int n) {
+        leftToRight(n);
+    }
+
+    // 请把1~N层圆盘 从左 -> 右
+    // 步骤2：
+    public static void leftToRight(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from left to right");
+            return;
+        }
+        leftToMid(n - 1);
+        System.out.println("Move " + n + " from left to right");
+        midToRight(n - 1);
+    }
+
+    // 请把1~N层圆盘 从左 -> 中
+    // 步骤3：
+    public static void leftToMid(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from left to mid");
+            return;
+        }
+        leftToRight(n - 1);
+        System.out.println("Move " + n + " from left to mid");
+        rightToMid(n - 1);
+    }
+    // 步骤4：
+    public static void rightToMid(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from right to mid");
+            return;
+        }
+        rightToLeft(n - 1);
+        System.out.println("Move " + n + " from right to mid");
+        leftToMid(n - 1);
+    }
+    // 步骤5：
+    public static void midToRight(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from mid to right");
+            return;
+        }
+        midToLeft(n - 1);
+        System.out.println("Move " + n + " from mid to right");
+        leftToRight(n - 1);
+    }
+    // 步骤6：
+    public static void midToLeft(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from mid to left");
+            return;
+        }
+        midToRight(n - 1);
+        System.out.println("Move " + n + " from mid to left");
+        rightToLeft(n - 1);
+    }
+    // 步骤7：
+    public static void rightToLeft(int n) {
+        if (n == 1) {
+            System.out.println("Move 1 from right to left");
+            return;
+        }
+        rightToMid(n - 1);
+        System.out.println("Move " + n + " from right to left");
+        midToLeft(n - 1);
+    }
+
+    // 方法2：
+    public static void hanoi2(int n) {
+        if (n > 0) {
+            func(n, "left", "right", "mid");
+        }
+    }
+
+    // 1~i 圆盘 目标是from -> to， other是另外一个
+    public static void func(int N, String from, String to, String other) {
+        if (N == 1) { // base
+            System.out.println("Move 1 from " + from + " to " + to);
+        } else {
+            func(N - 1, from, other, to);
+            System.out.println("Move " + N + " from " + from + " to " + to);
+            func(N - 1, other, to, from);
+        }
+    }
+
+    public static void main(String[] args) {
+        int n = 3;
+        hanoi1(n);
+        System.out.println("============");
+        hanoi2(n);
+        System.out.println("============");
+    }
+
+}
