e240df6d2d307e21ddc7d00e4ee157bf7a4b320e
zhouzhitong <528382226@qq.com>
2021-01-10 23:59:34 +0800

    spring aop 事务传播特性

--- maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/tx/propagation/BookService.java
+++ maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/tx/propagation/BookService.java
@@ -2,7 +2,6 @@ package com.zzt.tx.propagation;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.UnexpectedRollbackException;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -36,10 +35,28 @@ public class BookService {
      @param id    书的ID
      @param price 书的新价格
      */
-    @Transactional
+    @Transactional(propagation = Propagation.REQUIRES_NEW)
     public void updatePrice(int id, int price) {
         System.out.println("############### 222222 ###############");
         bookDao.updatePrice(id, price);
+//        int i = 1 / 0;
+    }
+
+    /**
+     在同一个类里面，调用多个方式[事务]时，其实就是同一个事务。
+     初步总结：同一个类中，方法之间的相互调用，只会开启一个事务...。
+     */
+    @Transactional
+    public void mulTx() {
+        /*try {
+        } catch (Exception e) {
+        }*/
+        updatePrice(1, 111);
+        checkout("zhangsan", 1);
+        try {
+        } catch (Exception e) {
+            System.out.println("错误信息：" + e.getClass() + " -- " + e.getMessage());
+        }
 //        int i = 1 / 0;
     }
 }
