9a1ac1f3f4ca5f0e659869ff5be4f142eb419431
zhouzhitong <528382226@qq.com>
2021-01-14 12:55:17 +0800

    spring - tx

--- maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/tx/propagation/MyTest.java
+++ maven-java-07-spring-code-5_2_9/src/main/java/com/zzt/tx/propagation/MyTest.java
@@ -6,12 +6,12 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class MyTest {
 
     public static void main(String[] args) {
-        ApplicationContext context = new ClassPathXmlApplicationContext("jdbcTemplate.xml");
-        BookService mulService = context.getBean(BookService.class);
-        mulService.mulTx();
         /*ApplicationContext context = new ClassPathXmlApplicationContext("jdbcTemplate.xml");
-        MulService mulService = context.getBean("mulService", MulService.class);
+        BookService mulService = context.getBean(BookService.class);
         mulService.mulTx();*/
+        ApplicationContext context = new ClassPathXmlApplicationContext("jdbcTemplate.xml");
+        MulService mulService = context.getBean("mulService", MulService.class);
+        mulService.mulTx();
     }
 
 }
